dd9be0e08f0f4768842c4d43c03ba6c24780f80a
Andrew Muro <andrew.t.muro@gmail.com>
2015-06-29 20:27:12 -0400

    added rbenv install

---
+++ rbenv-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.zshrc
+reset
