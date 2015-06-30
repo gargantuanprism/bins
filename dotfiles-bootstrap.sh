c96148ebdd6467c6a841a1202292aba5f5ec0156
Andrew Muro <andrew.t.muro@gmail.com>
2015-06-29 22:04:40 -0400

    changed ext

--- dotfiles-bootstrap.sh
+++ dotfiles-bootstrap.sh
@@ -8,6 +8,8 @@ ln -s $DOT/.xmonad $HOME/.xmonad
 ln -s $DOT/.emacs.d $HOME/.emacs.d
 ln -s $DOT/.emacs $HOME/.emacs
 ln -s $DOT/.fluxbox $HOME/.fluxbox
+ln -s $DOT/mc $HOME/.config/mc
+ln -s $DOT/.Xdefaults $HOME/.Xdefaults
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
