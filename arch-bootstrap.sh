453a9d9f3751e269089a375ee509161e5a7f61fd
Andrew Muro <andrew.t.muro@gmail.com>
2015-06-29 20:27:22 -0400

    added arch bootstrap

---
+++ arch-bootstrap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# base
+pacman -Sy --noconfirm \
+       zsh \
+       git \
+       emacs \
+       gnome-keyring \
+       networkmanager \
+       network-manager-applet \
+       parcellite \
+       binutils \
+       make \
+       patch \
+       fakeroot \
+       chromium \
+       rxvt-unicode \
+       dmenu \
+       fluxbox \
+       xterm \
+       yajl \
+       wget \
+       firefox \
+       vim \
+       lxdm \
+       clementine \
+       gcc \
+       deluge \
+       python2 \
+       python \
+       gnucash
+       
+# yaourt
+cd /tmp
+
+wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+
+tar xzf package-query.tar.gz
+tar xzf yaourt.tar.gz
+
+cd package-query
+makepkg -i
+
+cd ../yaourt
+makepkg -i
+
+# aur
+yaourt -S --noconfirm \
+       kupfer \
+       spotify \
+       google-chrome \
+       dropbox \
+       btsync
+
+# services
+systemctl enable NetworkManager
+systemctl start NetworkManager
