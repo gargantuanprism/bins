f3a408c552b42252927aca94f0f1722b6918be3a
Andrew Muro <andrew.t.muro@gmail.com>
2017-02-23 21:33:44 -0500

    updated bootstrap

--- dotfiles-bootstrap.sh
+++ dotfiles-bootstrap.sh
@@ -1,23 +1,40 @@
 #!/bin/zsh
 
-SRCS=(.tmux.conf \
-  .tmuxinator \
-  .fluxbox \
-  .Xdefaults \
-  .vimrc \
-  .vim \
-  .config/i3/.config \
-  .i3blocks.conf \
-  )
-DOT=$HOME/.dotfiles
 
 cd $HOME
 
-for src in $SRCS; do
-  ln -sf $DOT/$src $HOME/$src
-done
+# Dotfiles
+git clone https://github.com/0xANDREW/dotfiles.git .dotfiles
+
+DOT=$HOME/.dotfiles
+
+ln -sf $DOT/fluxbox .fluxbox
+ln -sf $DOT/i3 .config/i3
+ln -sf $DOT/polybar .config/polybar
+ln -sf $DOT/tmuxinator .tmuxinator
+ln -sf $DOT/xmonad .xmonad
+ln -sf $DOT/emacs .emacs
+ln -sf $DOT/i3blocks.conf .i3blocks.conf
+ln -sf $DOT/i3status.conf .i3status.conf
+ln -sf $DOT/profile .profile
+ln -sf $DOT/tmux.conf .tmux.conf
+ln -sf $DOT/vimrc .vimrc
+ln -sf $DOT/Xresources .Xresources
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Base16
+git clone https://github.com/chriskempson/base16-shell.git \
+  ~/.config/base16-shell
+
+# Prezto
+git clone --recursive https://github.com/0xandrew/prezto.git \
+  "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
